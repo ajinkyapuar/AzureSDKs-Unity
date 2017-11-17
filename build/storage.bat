5580fa4ac07949059388363fe004bf676a7b8904
Brian Peek <brian@brianpeek.com>
2017-11-17 12:48:41 -0500

    Add version to script

--- build/storage.bat
+++ build/storage.bat
@@ -3,6 +3,7 @@
 REM Set paths
 set storagedir=..\..\azure-storage-net
 set sampledir=..\Storage
+set version=8.6.0
 
 REM Build Release binaries
 msbuild %storagedir%\Lib\Unity\Microsoft.WindowsAzure.Storage.Unity.csproj /t:Rebuild /p:Configuration=Release
@@ -13,6 +14,6 @@ copy /y %storagedir%\Lib\Unity\bin\Release\*.dll %sampledir%\Assets\Plugins
 copy /y %storagedir%\Lib\WindowsRuntime\bin\Release\*.dll %sampledir%\Assets\Plugins\WSA
 
 REM Package
-"C:\Program Files\Unity2017.1\Editor\Unity.exe" -batchmode -projectPath %cd%\%sampledir% -exportPackage Assets %cd%\azure-storage-unity.unitypackage -quit
+"C:\Program Files\Unity2017.1\Editor\Unity.exe" -batchmode -projectPath %cd%\%sampledir% -exportPackage Assets %cd%\azure-storage-unity-%version%.unitypackage -quit
 
 echo "Packaging complete."
