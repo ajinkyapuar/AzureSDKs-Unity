1e65d04b4ea695c1991cb2fcbaf156b35cbe0110
Brian Peek <brian@brianpeek.com>
2017-11-17 12:44:34 -0500

    Update build script and binaries for sample

--- build/storage.bat
+++ build/storage.bat
@@ -8,21 +8,11 @@ REM Build Release binaries
 msbuild %storagedir%\Lib\Unity\Microsoft.WindowsAzure.Storage.Unity.csproj /t:Rebuild /p:Configuration=Release
 msbuild %storagedir%\Lib\WindowsRuntime\Microsoft.WindowsAzure.Storage.RT.csproj /t:Rebuild /p:Configuration=Release
 
-REM Remove any old artifacts
-rmdir /s /q Packaging
-
-REM Create the directory structure
-mkdir Packaging\Assets\Plugins\WSA
-mkdir Packaging\Assets\Sample
-mkdir Packaging\Assets\StreamingAssets
-
-REM Copy the binaries and sample
-copy /y %storagedir%\Lib\Unity\bin\Release\*.dll Packaging\Assets\Plugins
-copy /y %storagedir%\Lib\WindowsRuntime\bin\Release\*.dll Packaging\Assets\Plugins\WSA
-copy /y %sampledir%\Assets\Sample\*.* Packaging\Assets\Sample
-copy /y %sampledir%\Assets\StreamingAssets\*.* Packaging\Assets\StreamingAssets
+REM Copy binaries to sample
+copy /y %storagedir%\Lib\Unity\bin\Release\*.dll %sampledir%\Assets\Plugins
+copy /y %storagedir%\Lib\WindowsRuntime\bin\Release\*.dll %sampledir%\Assets\Plugins\WSA
 
 REM Package
-"C:\Program Files\Unity2017.1\Editor\Unity.exe" -batchmode -projectPath %cd%\Packaging -exportPackage Assets %cd%\azure-storage-unity.unitypackage -quit
+"C:\Program Files\Unity2017.1\Editor\Unity.exe" -batchmode -projectPath %cd%\%sampledir% -exportPackage Assets %cd%\azure-storage-unity.unitypackage -quit
 
 echo "Packaging complete."
