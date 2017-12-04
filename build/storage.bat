70bafc3d223486a076a87e3add9e48e463cce466
Brian Peek <BrianPeek@users.noreply.github.com>
2017-12-04 16:00:04 -0500

    Update storage.bat

--- build/storage.bat
+++ build/storage.bat
@@ -3,6 +3,7 @@
 REM Set paths
 set storagedir=..\..\azure-storage-net
 set sampledir=..\Storage
+set unityexe="C:\Program Files\Unity\Editor\Unity.exe"
 set version=8.6.0
 
 REM Build Release binaries
@@ -14,6 +15,6 @@ copy /y %storagedir%\Lib\Unity\bin\Release\*.dll %sampledir%\Assets\Plugins
 copy /y %storagedir%\Lib\WindowsRuntime\bin\Release\*.dll %sampledir%\Assets\Plugins\WSA
 
 REM Package
-"C:\Program Files\Unity2017.1\Editor\Unity.exe" -batchmode -projectPath %cd%\%sampledir% -exportPackage Assets %cd%\azure-storage-unity-%version%.unitypackage -quit
+%unityexe% -batchmode -projectPath %cd%\%sampledir% -exportPackage Assets %cd%\azure-storage-unity-%version%.unitypackage -quit
 
 echo "Packaging complete."
