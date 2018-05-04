e125ad1903ee04ace6f18a01ae8a2aed8bcae4db
Brian Peek <brian@brianpeek.com>
2018-05-04 17:06:06 -0400

    Add file method

--- Microsoft.ProjectOxford.Vision/Assets/AzureSamples/Microsoft.ProjectOxford.Vision/VisionTest.cs
+++ Microsoft.ProjectOxford.Vision/Assets/AzureSamples/Microsoft.ProjectOxford.Vision/VisionTest.cs
@@ -1,30 +1,73 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using Microsoft.ProjectOxford.Vision;
 using Microsoft.ProjectOxford.Vision.Contract;
+using UnityEngine;
+
+#if WINDOWS_UWP && ENABLE_DOTNET
+using Windows.Storage;
+using Windows.Storage.Streams;
+#endif
 
 public class VisionTest : BaseVision
 {
-    private static VisionServiceClient visionClient;
-    
-    // Use this for initialization
-    public async void TestVision()
-    {
-        try
-        {
+	private static VisionServiceClient visionClient;
+
+	public string ImageToUpload = "islands.png";
+
+	// Use this for initialization
+	public async void TestVision()
+	{
+		try
+		{
 			VisionServiceClient client = new VisionServiceClient(VisionKey, VisionEndpoint);
-			AnalysisResult result = await client.DescribeAsync("https://upload.wikimedia.org/wikipedia/commons/f/f3/ISS-52_Aurora_australis_above_Antarctica.jpg");
-			WriteLine(result.Description.Captions[0].Text);
-			string tags = result.Description.Tags.Aggregate((x, y) => x + ", " + y);
-			WriteLine(tags);
-        }
-        catch (ClientException ex)
-        {
-            WriteLine(ex.Error.Message);
-        }
-        catch (Exception ex)
-        {
-            WriteLine(ex.ToString());
-        }
-    }
+			AnalysisResult result = await client.DescribeAsync("https://upload.wikimedia.org/wikipedia/commons/7/76/Las_Vegas_strip.jpg");
+			WriteOutput(result);
+		}
+		catch (ClientException ex)
+		{
+			WriteLine(ex.Error.Message);
+		}
+		catch (Exception ex)
+		{
+			WriteLine(ex.ToString());
+		}
+	}
+
+	public async void TestVisionFile()
+	{
+		Stream s;
+
+		try
+		{
+#if WINDOWS_UWP && ENABLE_DOTNET
+			StorageFolder storageFolder = await StorageFolder.GetFolderFromPathAsync(Application.streamingAssetsPath.Replace('/', '\\'));
+			StorageFile sf = await storageFolder.GetFileAsync(ImageToUpload);
+			IRandomAccessStreamWithContentType ras = await sf.OpenReadAsync();
+			s = ras.AsStreamForRead();
+#else
+			s = new FileStream(Path.Combine(Application.streamingAssetsPath, ImageToUpload), FileMode.Open);
+#endif
+
+			VisionServiceClient client = new VisionServiceClient(VisionKey, VisionEndpoint);
+			AnalysisResult result = await client.DescribeAsync(s);
+			WriteOutput(result);
+		}
+		catch (ClientException ex)
+		{
+			WriteLine(ex.Error.Message);
+		}
+		catch (Exception ex)
+		{
+			WriteLine(ex.ToString());
+		}
+	}
+
+	private void WriteOutput(AnalysisResult result)
+	{
+		WriteLine(result.Description.Captions[0].Text);
+		string tags = result.Description.Tags.Aggregate((x, y) => x + ", " + y);
+		WriteLine(tags);
+	}
 }
