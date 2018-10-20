89925245fc02d0b5b6da2cbb8966365ff0a8d9e6
Brian Peek <brian@brianpeek.com>
2018-10-19 20:49:32 -0400

    updates...

---
+++ _build/build.bat
@@ -0,0 +1,3 @@
+dotnet NuGet2Unity.dll -n Microsoft.Azure.EventHubs -p ..\Microsoft.Azure.EventHubs -o "..\_UnityPackages" -m
+dotnet NuGet2Unity.dll -n Microsoft.Azure.Mobile.Client -p ..\Microsoft.Azure.Mobile.Client -o "..\_UnityPackages" -m
+dotnet NuGet2Unity.dll -n WindowsAzure.Storage -p ..\WindowsAzure.Storage -o "..\_UnityPackages" -m
