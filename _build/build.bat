ab81d27f89aa8514d8c8edf0151cf9bc63316b6c
Brian Peek <brian@brianpeek.com>
2018-10-19 23:41:26 -0400

    Make NuGet2Unity a submodule

--- _build/build.bat
+++ _build/build.bat
@@ -1,3 +1,8 @@
-dotnet NuGet2Unity.dll -n Microsoft.Azure.EventHubs -p ..\Microsoft.Azure.EventHubs -o "..\_UnityPackages" -m
-dotnet NuGet2Unity.dll -n Microsoft.Azure.Mobile.Client -p ..\Microsoft.Azure.Mobile.Client -o "..\_UnityPackages" -m
-dotnet NuGet2Unity.dll -n WindowsAzure.Storage -p ..\WindowsAzure.Storage -o "..\_UnityPackages" -m
+@echo off
+
+pushd NuGet2Unity\src
+dotnet build
+dotnet run -- -n Microsoft.Azure.EventHubs -p ..\..\..\Microsoft.Azure.EventHubs -o ..\..\..\_UnityPackages -m
+dotnet run -- -n Microsoft.Azure.Mobile.Client -p ..\..\..\Microsoft.Azure.Mobile.Client -o ..\..\..\_UnityPackages -m
+dotnet run -- -n WindowsAzure.Storage -p ..\..\..\WindowsAzure.Storage -o ..\..\..\_UnityPackages -m
+popd
